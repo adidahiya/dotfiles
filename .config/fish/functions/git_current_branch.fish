398d4625c3df1e974d6d2bb74de999b4e68e99ed
Adi Dahiya <adi.dahiya14@gmail.com>
2021-08-23 14:47:26 -0400

    Update git_current_branch.fish
    `^` is no longer valid syntax, see https://stackoverflow.com/questions/68434304/seeing-fatal-refusing-to-point-head-outside-of-refs-after-every-command-i-ru

--- .config/fish/functions/git_current_branch.fish
+++ .config/fish/functions/git_current_branch.fish
@@ -1,3 +1,3 @@
 function git_current_branch -d "Display name of current git branch"
-  git symbolic-ref HEAD ^/dev/null | awk -F/ '{print $3;}'
+  git symbolic-ref HEAD 2>/dev/null | awk -F/ '{print $3;}'
 end
