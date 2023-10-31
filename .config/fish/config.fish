9fd311fd08eccb749709d6c60f2fd2fe0a66615c
Adi Dahiya <adi.dahiya14@gmail.com>
2023-10-31 19:52:21 -0400

    Update fish functions & config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,27 +17,11 @@ function yarn-tools-fix-duplicates
   yarn-tools fix-duplicates yarn.lock > fixed-yarn.lock ;and rm yarn.lock ;and mv fixed-yarn.lock yarn.lock
 end
 
-# Base16 Shell
-if status --is-interactive
-  eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
-end
-
-function nvm
-  bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
-end
-
 set -x NVM_DIR ~/.nvm
-nvm use default --silent
+load_nvm > /dev/stderr
 
 set fish_greeting ""
 set -U BROWSER "open"
 set -x EDITOR "vim"
-set -x JAVA_HOME (/usr/libexec/java_home -v "11")
-set -x GOPATH /usr/local/share/go
 set -x PATH /usr/local/bin $PATH
-# set -x PATH (yarn global bin) $PATH
-# set -x PATH $HOME/.yarn/bin $PATH
-set -x PATH $HOME/.config/yarn/global/node_modules/.bin $PATH
-
-# Remove this to upgrade node to a different major version
-# set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
+set -x PATH /opt/homebrew/bin/ $PATH
